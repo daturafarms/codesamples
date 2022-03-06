ddfcbbd7420f9d3e80230298a2e8709967c09fc9
daturafarms <97363837+daturafarms@users.noreply.github.com>
2022-03-05 22:08:41 -0500

    Create largest_perimeter_triangle.py
    codecademy stuff

---
+++ largest_perimeter_triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        a, b, c = nums.pop(), nums.pop(), nums.pop()
+        while b+c<=a:
+            if not nums:
+                return 0
+            a, b, c = b, c, nums.pop()
+        return a + b + c
